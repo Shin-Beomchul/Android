8aa38696d94dc8eddfba3139317a5314c7e9cd69
Shun Beom-chul <sbc0830g@gmail.com>
2018-03-16 18:09:35 +0900

    add -> API Module

--- MeterialCalc/app/src/main/java/materialcalc/house/godbeom/com/materialcalc/App.java
+++ MeterialCalc/app/src/main/java/materialcalc/house/godbeom/com/materialcalc/App.java
@@ -4,6 +4,9 @@ import android.support.multidex.MultiDexApplication;
 
 import com.google.firebase.FirebaseApp;
 
+import materialcalc.house.godbeom.com.retrofit2.core.API;
+import materialcalc.house.godbeom.com.retrofit2.define.Server;
+
 /**
  * Created by Administrator on 2018-03-02.
  */
@@ -14,5 +17,7 @@ public class App extends MultiDexApplication {
 	public void onCreate() {
 		super.onCreate();
 		FirebaseApp.initializeApp(this);
+		Server serverType = Server.valueOf(BuildConfig.SERVER_TYPE);
+		API.instance().initialize(this, serverType);
 	}
 }
--- MeterialCalc/app/src/main/java/materialcalc/house/godbeom/com/materialcalc/sample/aspectj/ActAspectJSample.java
+++ MeterialCalc/app/src/main/java/materialcalc/house/godbeom/com/materialcalc/sample/aspectj/ActAspectJSample.java
@@ -6,33 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import materialcalc.house.godbeom.com.materialcalc.R;
 import materialcalc.house.godbeom.com.materialcalc.sample.aspectj.threadinjector.bBackground;
 import materialcalc.house.godbeom.com.materialcalc.sample.aspectj.threadinjector.bUiThread;
 
 public class ActAspectJSample extends AppCompatActivity implements View.OnClickListener {
-
-
-	@BindView(R.id.ClickMe)
-	Button ClickMe;
-
-	@BindView(R.id.btnMainHooker)
-	Button btnMainHooker;
-
-	@BindView(R.id.btnSubHooker)
-	Button btnSubHooker;
-
-	@BindView(R.id.tv)
-	TextView tv;
-
-	@BindView(R.id.tvUiObject)
-	TextView tvUiObject;
-
+	@BindView(R.id.tv) TextView tv;
+	@BindView(R.id.tvUiObject) TextView tvUiObject;
 
 	StringBuilder sb = new StringBuilder();
 
@@ -41,28 +26,34 @@ public class ActAspectJSample extends AppCompatActivity implements View.OnClickL
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_act_aspect_jsample);
 		ButterKnife.bind(this);
-		ClickMe.setOnClickListener(this);
-		btnMainHooker.setOnClickListener(this);
-		btnSubHooker.setOnClickListener(this);
+
 
 		Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate); //무한회전 (Ui Thread 상태 확인)
 		tvUiObject.setAnimation(animation);
 	}
 
 
-	@Override
+	@OnClick({R.id.ClickMe,
+			R.id.btnMainHooker,
+			R.id.btnSubHooker,
+			R.id.delayPostUi})
 	public void onClick(View view) {
 		int id = view.getId();
 
 		switch (id) {
 			case R.id.ClickMe:
-				tv.setText(sb.append("클릭 !").append("\n").toString());
+				tv.setText(sb.append(" ** 클릭 **  !").append("\n").toString());
 				break;
 			case R.id.btnMainHooker:
 				delayedUiThread(2000);
 				break;
 			case R.id.btnSubHooker:
 				subThreadStart();
+				break;
+
+			case R.id.delayPostUi:
+				tv.setText(sb.append("2초 후 문구를 확인 하세요.").append("\n"));
+				postUiDelayed();
 				break;
 		}
 	}
@@ -102,9 +93,8 @@ public class ActAspectJSample extends AppCompatActivity implements View.OnClickL
 	}
 
 
-	/**2초간 Ui 정지.
+	/**2초간 Ui Thread 정지.
 	 * */
-
 	@bUiThread
 	public void delayedUiThread(int uiBlockTime){
 		tv.setText(sb.append("BlockUiThread "+uiBlockTime +"Ms").append("\n").toString());
@@ -113,6 +103,13 @@ public class ActAspectJSample extends AppCompatActivity implements View.OnClickL
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
 	}
+
+
+	@bUiThread(delay = 2000)
+	public void postUiDelayed(){
+		tv.setText(sb.append("버튼을 누르고 2초 후 프린트 되었습니다.").append("\n").toString());
+	}
+
+
 }
